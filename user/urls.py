ce738a36c98cf0ccc499d035b9b687a3220da1ef
Rohit Kumar <rohitmishra051000@gmail.com>
2022-02-15 19:40:08 +0530

    User changes

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import LoginView, OTPView, LogoutView, StudentAPI, EditAPI
+from .views import LoginView, OTPView, LogoutView, UserView, UpdateView
 
 urlpatterns = [
     path('login', LoginView.as_view()),
     path('otp', OTPView.as_view()),
     path('logout', LogoutView.as_view()),
-    path('viewprofile',StudentAPI.as_view()),
-    path('editprofile',EditAPI.as_view()),
+    path('viewprofile', UserView.as_view()),
+    path('editprofile', UpdateView.as_view()),
 ]
