e944fdc3b893c23417b05c025595f2e7e84fb4c8
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-03-11 19:58:27 +0530

    cron api added

--- courses/views.py
+++ courses/views.py
@@ -184,3 +184,56 @@ class VideoWatched(APIView):
         }
         return response 
 
+class ProcessCreditView(APIView):
+    def update_credit(userid, price):
+        Q = f"SELECT * FROM `credit_referrer_referee` where `referee_id` = {userid};"
+        cursor  = connection.cursor()
+        cursor.execute(Q)
+        rows = cursor.fetchall()
+        print(rows)
+        if (len(rows) > 0):
+            for row in rows:
+                print(row)
+                if (row[2] == False):
+                    amount = price * 0.02
+                    # Credit.objects.create(userid = row[1], amount = amount, referee = userid)
+                elif (row[2] == True):
+                    amount = price * 0.05
+                    # Credit.objects.create(userid = row[1], amount = amount, referee = userid)
+                Q2 = f"INSERT INTO `credit_credit` ( `userid_id`, `amount`, `referee`) VALUES ('{row[1]}', '{amount}', '{userid}');"
+                cursor.execute(Q2)
+        else:
+            pass
+
+    def create_video_user(course_id,user_id):
+        Q = f"SELECT `id` FROM courses_video where `course_id` ={course_id};"
+        cursor  = connection.cursor()
+        cursor.execute(Q)
+        rows = cursor.fetchall()
+        now  = datetime.datetime.now()
+        for row in rows:
+            Q2 = f"INSERT INTO `courses_videouser` ( `date_purchased`, `is_watched`, `userid_id`, `videoid_id`) VALUES ('{now}', '0', '{user_id}','{row[0]}');"
+            cursor.execute(Q2)
+        cursor.close()
+        return
+
+    def post(self,request):
+        Q = "SELECT `courseid_id`,`userid_id` FROM courses_courseuser where `is_verified` = 1 and `is_processed` = 0;"
+        cursor  = connection.cursor()
+        cursor.execute(Q)
+        rows = cursor.fetchall()
+        if (len(rows) > 0):
+            for row in rows:
+                user_id =   row[1]
+                course_id = row[0]
+                course = Course.objects.get(id = course_id)
+                price = course.price
+                print(price)
+                # print(course_id, user_id)
+                self.create_video_user(course_id,user_id)
+                self.update_credit(user_id,price)
+            CourseUser.objects.filter(is_verified = True, is_processed = False).update(is_processed = True)
+        else:
+            pass
+        response = Response()
+        return response
